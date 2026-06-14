f8c5c8519384f1d069b3c9df013bcaa7cbc5463a
Teddy Li <teddyxlandlee@hotmail.com>
2026-06-14 15:23:16 +0800

    Missing `this` context... oh hell, this is quite different from Java or Python

--- src/access-token.ts
+++ src/access-token.ts
@@ -39,10 +39,9 @@ export function newAccessToken(payload: TokenIdentity): string {
 export function verifyAccessToken(token: string): null | TokenIdentity {
     try {
         const payload = jwt.verify(token, JWT_ACCESS_TOKEN) as string | (RealPayload & jwt.JwtPayload)
-        console.log('[PAYLOAD]', JSON.stringify(payload))
         if (typeof payload === 'string') return null    // invalid
         const scopes = payload.scopes
-        if (!Array.isArray(scopes) || !scopes.every(SCOPES.includes)) return null    // invalid scope
+        if (!Array.isArray(scopes) || !scopes.every(x => SCOPES.includes(x))) return null    // invalid scope
         return {
             scopes,
             userId: payload.sub,
